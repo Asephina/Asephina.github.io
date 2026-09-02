52e0155296b6cd0470eae381ad92a6f35165b015
Asephina <1148086056@qq.com>
2026-09-02 17:59:22 +0800

    54343

--- web/viewer.mjs
+++ web/viewer.mjs
@@ -14565,25 +14565,10 @@ initCom(PDFViewerApplication);
   PDFPrintServiceFactory.initGlobals(PDFViewerApplication);
 }
 {
-  const HOSTED_VIEWER_ORIGINS = ["null", "http://mozilla.github.io", "https://mozilla.github.io"];
   var validateFileURL = function (file) {
+    // 自部署版本，跳过 origin 校验，确保所有来源的 PDF 都能正常加载
     if (!file) {
       return;
-    }
-    try {
-      const viewerOrigin = new URL(window.location.href).origin || "null";
-      if (HOSTED_VIEWER_ORIGINS.includes(viewerOrigin)) {
-        return;
-      }
-      const fileOrigin = new URL(file, window.location.href).origin;
-      if (fileOrigin !== viewerOrigin) {
-        throw new Error("file origin does not match viewer's");
-      }
-    } catch (ex) {
-      PDFViewerApplication._documentError("pdfjs-loading-error", {
-        message: ex.message
-      });
-      throw ex;
     }
   };
   var onFileInputChange = function (evt) {
